237b7420e19712e325b4fcedd13a7d039f3447ba
Andriiko <lesiv433@gmail.com>
2024-10-31 10:07:11 +0200

    ping endpoint

---
+++ src/Web/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok("Dogshouseservice.Version1.0.1");
+    }
+}
