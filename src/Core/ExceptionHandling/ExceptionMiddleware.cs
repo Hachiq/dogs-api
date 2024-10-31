44f0de050ac6e975e3d2a9c77a2380192cc65ba2
Andriiko <lesiv433@gmail.com>
2024-10-31 18:49:12 +0200

    handle json exception

--- src/Core/ExceptionHandling/ExceptionMiddleware.cs
+++ src/Core/ExceptionHandling/ExceptionMiddleware.cs
@@ -17,11 +17,33 @@ public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddlewa
         {
             await _next(context);
         }
+        catch (JsonException ex)
+        {
+            logger.LogWarning("Something went wrong: {ex}", ex);
+            await HandleJsonExceptionAsync(context, ex);
+        }
         catch (Exception ex)
         {
             logger.LogError("Something went wrong: {ex}", ex);
             await HandleExceptionAsync(context, ex);
         }
+    }
+
+    private async Task HandleJsonExceptionAsync(HttpContext context, Exception exception)
+    {
+        var handleExceptionModel = new HandleExceptionModel
+        {
+            Code = HttpStatusCode.BadRequest,
+            Message = "Invalid JSON format",
+            Details = exception.Message
+        };
+
+        var responseContent = JsonSerializer.Serialize(handleExceptionModel);
+
+        context.Response.ContentType = ApplicationJsonType;
+        context.Response.StatusCode = (int)handleExceptionModel.Code;
+
+        await context.Response.WriteAsync(responseContent);
     }
 
     private Task HandleExceptionAsync(HttpContext context, Exception exception)
